5a9e5040dfbb7a1c50002beb50c30895ce26678b
Nikoleta <nikolpant2@gmail.com>
2023-11-02 17:30:50 +0100

    register page

--- LearnLanguages-Front/src/app/pages/login-page/login-page.component.ts
+++ LearnLanguages-Front/src/app/pages/login-page/login-page.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
-
-export class AppModule { }
+export class AppModule {}
 @Component({
   selector: 'app-login',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
-
-constructor(private formConstructor: FormBuilder) { }
+  constructor(private formConstructor: FormBuilder) {}
 
   loginForm = this.formConstructor.group({
     username: [''],
     password: ['', [Validators.required, Validators.minLength(8)]],
-  })
+  });
 
   onSubmit() {
-    console.log(this.loginForm)
+    console.log(this.loginForm);
+  }
+
+  getRandomPosition(): object {
+    const top = `${80 + Math.random() * 60}%`;
+    const left = `${20 + Math.random() * 60}%`;
+
+    const zIndex = -1;
+    const margin = `${Math.random() * 20}px`;
+
+    return { margin, top, left, zIndex, position: 'absolute' };
   }
 }
